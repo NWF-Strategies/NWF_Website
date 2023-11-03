7d58025260ac20b491d0e3fdb982351dbc0a69e5
Khoa <ktran7@stevens.edu>
2023-11-03 11:08:18 -0400

    added first section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>
+    <nav className='w-full flex py-6 justify-between items-center navbar border-b-[2px] border-white bg-black'>
       <img src={polygon} alt='nwf' className='w-[124px] h-[32px]' />
 
       <img src={logo} alt='nwf' className='w-[124px] h-[32px]' />
--- src/pages/Solutions.tsx
+++ src/pages/Solutions.tsx
@@ -2,23 +2,30 @@ import React from 'react'
 import { solutions_tags } from '../constants';
 import {Navbar, Footer} from '../components';
 import styles from '../style';
+import { gear } from '../assets';
 const Solutions = () => {
   return (
-    <div className='bg-black-gradient'>
+    <div className='blue__black__gradient'>
         <Navbar/>
-        <div className='h-[100hv]'>
-            <div>
-                <h1 className={`text-white`}>Solutions</h1>
-                <p className={`text-white`}>We combine traditional outreach strategy with innovative technology.</p>
+        <div className='relative h-[100hv] flex flex-col justify-center items-center z-10 mt-10'>
+            <img src={gear} alt="gear" className='absolute left-0 overflow-hidden z-0'/>
+            <div className='w-[100%] flex flex-col justify-center items-left md:ml-[700px] z-10'>
+                <div>
+                    <h1 className={`text-[#43BCCD] font-poppins text-[100px] font-bold leading-normal text-left `}>Solutions</h1>
+                    <p className={`text-white font-poppins text-[33px] font-normal leading-normal `}>We combine traditional outreach strategy with innovative technology.</p>
+                </div>
+                <ul className='md:ml-[100px] mt-10'>
+                    {solutions_tags.map((item) =>(
+                        <li className='flex flex-row justify-start items-center w-full'>
+                            <img src={item.img}/>
+                            <p className={`text-white font-poppins text-[33px] font-normal leading-normal text-center ml-5`}>{item.title}</p>
+                        </li>
+                    ))}
+                </ul>
             </div>
-            <ul>
-                {solutions_tags.map((item) =>(
-                    <li className='flex flex-row'>
-                        <img src={item.img}/>
-                        <p className={`${styles.paragraph} text-white`}>{item.title}</p>
-                    </li>
-                ))}
-            </ul>
+            <div>
+                
+            </div>
         </div>
         <Footer />
     </div>

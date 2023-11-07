73d8c4aaf61f9b388e60d95a067f39e910c5c6e8
Khoa <Kvtran3546@gmail.com>
2023-11-07 16:37:09 -0500

    styles the first section

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,9 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Newsletter from "./Newsletter";
 
-export { Navbar, Footer };
+export { 
+    Navbar, 
+    Footer,
+    Newsletter
+ };

9ab820c3b28dfe3214d824e70528cac6a818b3ed
Allison Jiang <allison.x.jiang@gmail.com>
2023-11-12 14:36:52 -0600

    More merging edits

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { close, logo, polygon, menu } from "../assets";
 import { navLinks } from "../constants";
 
@@ -8,17 +8,20 @@ const Navbar = () => {
 
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={polygon} alt='nwf' className='w-[124px] h-[32px]' />
+      <Link to={"/home"}>
+        <img src={polygon} alt='nwf' className='w-[124px] h-[32px]' />
+      </Link>
 
-      <img src={logo} alt='nwf' className='w-[124px] h-[32px]' />
+      <Link to={"/home"}>
+        <img src={logo} alt='nwf' className='w-[124px] h-[32px]' />
+      </Link>
 
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`font-poppins
-              font-normal cursor-pointer text-[16px] ${
-                index === navLinks.length - 1 ? "mr-0" : "mr-10"
+              font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mr-10"
               } text-white`}
           >
             <Link to={`/${nav.id}`}>{nav.title}</Link>
@@ -35,17 +38,15 @@ const Navbar = () => {
         />
 
         <div
-          className={`${
-            toggle ? "flex" : "hidden"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          className={`${toggle ? "flex" : "hidden"
+            } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
           <ul className='list-none flex-col flex justify-end items-center flex-1'>
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
                 className={`font-poppins
-                  font-normal cursor-pointer text-[16px] ${
-                    index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                  font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mb-4"
                   } text-white`}
               >
                 <a href={`#${nav.id}`}>{nav.title}</a>

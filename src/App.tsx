08036a13a02bff20c8fa95ef1856c9889a7a9a81
Allison Jiang <allison.x.jiang@gmail.com>
2023-11-17 16:28:35 -0600

    Created default padding wrapper for pages.

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,31 @@ import styles from "./style";
 // ... import other pages as well
 
 function App() {
+  const DefaultPadding: React.FC<{children: React.ReactNode}> = ({ children }) => (
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        {children}
+      </div>
+    </div>
+  );
+
   return (
     <div className='bg-black-gradient'>
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Router>
-            <Navbar />
-            <Routes>
-              <Route path='/solutions' element={<Solutions />} />
-              {/* Add routes for other pages similarly */}
-              <Route path='/about' element={<About />} />
-              <Route path='/careers' element={<Careers />} />
-              <Route path='/contactus' element={<ContactUs />} />
-              {/* Default route, typically Home */}
-              <Route path='/' element={<Home />} />
-            </Routes>
-            <Footer />
-          </Router>
-        </div>
-      </div>
+      <Router>
+        <DefaultPadding><Navbar /></DefaultPadding>
+
+        <Routes>
+            <Route path='/solutions' element={<DefaultPadding><Solutions /></DefaultPadding>} />
+            <Route path='/about' element={<DefaultPadding><About /></DefaultPadding>} />
+            <Route path='/careers' element={<DefaultPadding><Careers /></DefaultPadding>} />
+            <Route path='/contactus' element={<DefaultPadding><ContactUs /></DefaultPadding>} />
+            
+            {/* Default route, typically Home */}
+            <Route path='/' element={<Home />} />
+        </Routes>
+
+        <DefaultPadding><Footer /></DefaultPadding>
+      </Router>
     </div>
   );
 }

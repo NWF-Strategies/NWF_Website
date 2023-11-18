69c1af81eff18e41904f59842735383669753ffe
Khoa <Kvtran3546@gmail.com>
2023-11-17 21:01:34 -0500

    solution page and navbar/footer fixed

--- src/pages/Solutions.tsx
+++ src/pages/Solutions.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { solutions_tags } from "../constants";
+import { solutions_tags, solutions_notes } from "../constants";
 import { Navbar, Footer, Newsletter } from "../components";
 import styles from "../style";
 import { gear } from "../assets";
 const Solutions = () => {
   return (
     <div className=''>
-      <div className='blue__black__gradient relative h-[100vh] flex flex-col items-center z-10'>
+      <div className='relative lg:h-[100vh] h-fit py-[100px] blue__black__gradient flex flex-col items-center z-10'>
         <img
           src={gear}
           alt='gear'
-          className='absolute left-0 overflow-hidden z-0 lg:w-100 md:w-50 md:top-20 sm:top-10'
+          className='absolute left-0 overflow-hidden -z-10 lg:w-100 md:w-50 md:top-0 sm:top-0'
         />
-        <div className='absolute w-[100%] flex flex-col justify-center items-left lg:ml-[700px] md:ml-[200px] sm:ml-[200px] z-10 lg:top-[150px] md:top-[150px] sm:top-[150px]'>
+        <div className='w-[100%] flex flex-col justify-center items-left lg:ml-[30%] md:ml-[30%] sm:ml-[30%] ml-[30%] z-10 lg:top-[150px] md:top-[150px] sm:top-[150px]'>
           <div>
             <h1
               className={`text-[#43BCCD] font-poppins lg:text-[100px] md:text-[50px] sm:text-[40px] font-bold leading-normal text-left `}
@@ -20,7 +20,7 @@ const Solutions = () => {
               Solutions
             </h1>
             <p
-              className={`text-white font-poppins lg:text-[33px] md:text-[30px] sm:text-[22px] font-normal leading-normal `}
+              className={`text-white font-poppins lg:text-[25px] md:text-[20px] sm:text-[15px] font-normal leading-normal `}
             >
               We combine traditional outreach strategy with innovative
               technology.
@@ -40,8 +40,26 @@ const Solutions = () => {
           </ul>
         </div>
       </div>
-      <div className='teal__black__gradient'>
-        <div className='h-[1000px]'></div>
+      <div className='teal__black__gradient pb-10'>
+        <div className='h-fit py-10 px-10 mb-10'>
+          <ul>
+            {solutions_notes.map((item, index) => (
+              <li className="h-fit flex flex-row w-full py-1">
+                <div  className={`flex justify-center items-center w-1/2 ${index % 2 === 0 ? 'order-first' : 'order-last'} px-10`}>
+                  <img src={item.img} className="scale-[80%]"/>
+                </div>
+                <div className="w-1/2 flex flex-col justify-center items-start">
+                  <h3 className="text-[#43BCCD] font-poppins text-[20px] font-semibold leading-normal tracking-widest">Solution {index + 1}</h3>
+                  <div className="flex flex-row items-center justify-start w-full">
+                    <img src={item.icon} className="w-[60px] h-[60px]"/>
+                    <h3 className="ml-3 text-white font-poppins lg:text-[45px] md:text-[30px] sm:text-[20px] font-normal leading-normal tracking-normal">{item.title}</h3>
+                  </div>
+                  <p className="text-white lg:text-[23px] md:text-[15px] sm:text-[13px] font-normal font-poppins">{item.description}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
         <Newsletter />
       </div>
     </div>

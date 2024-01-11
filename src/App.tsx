a18a9ccb1afad3573148aae40e0649758a986026
Bryan Arius <bryanarius@gmail.com>
2024-01-11 04:03:20 -0500

    Add error page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Solutions, About, Careers, ContactUs, Home } from "./pages/";
+import { NotFound, Solutions, About, Careers, ContactUs, Home } from "./pages/";
 import { Navbar, Footer } from "./components";
+
 // ... import other pages as well
 
 function App() {
@@ -18,13 +19,12 @@ function App() {
         <Navbar />
 
         <Routes>
+          <Route path='/' element={<Home />} />
           <Route path='/solutions' element={<Solutions />} />
           <Route path='/about' element={<About />} />
           <Route path='/careers' element={<Careers />} />
           <Route path='/contactus' element={<ContactUs />} />
-
-          {/* Default route, typically Home */}
-          <Route path='/' element={<Home />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
 
         <Footer />
